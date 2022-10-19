28f7e323d49ab1df8ed87b4704caa0f348d1f94a
RoyalBae <chidinma.sylvia219@gmail.com>
2022-10-19 16:49:51 -0700

    Adpha 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,18 +19,23 @@ void print_times_table(int n)
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
+
 				_putchar(' ');
 
 				prod = numb * mult
 
 				if (prod <= 99)
+
 					_putchar(' ');
+
 				if (prod <= 9)
+
 					_putchar(' ');
 
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
+
 					_putchar(((prod / 10)) % 10 + '10');
 				}
 				else if (prod <= 99 && prod >= 10)
