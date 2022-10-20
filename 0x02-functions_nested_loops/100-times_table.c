d2c3b92ab8ec574a25c993b00ee99624ef8ea29d
RoyalBae <chidinma.sylvia219@gmail.com>
2022-10-19 17:06:37 -0700

    Adpha 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - It print the times table
+ * print_times_table - It prints the times table
  * of the input starting from 0
  * @n: The value of the times table to be printed
  */
@@ -19,23 +19,19 @@ void print_times_table(int n)
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
-
 				_putchar(' ');
 
 				prod = numb * mult;
 
 				if (prod <= 99)
-
 					_putchar(' ');
 
 				if (prod <= 9)
-
 					_putchar(' ');
 
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-
 					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
