6f9c3597d7859ef59a273b0fc17799f7c096c0c3
RoyalBae <chidinma.sylvia219@gmail.com>
2022-10-19 19:37:53 -0700

    Adpha 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print the first 50 Fibonacci numbers,
+ * starting with 1 and 2
+ * and followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int c, f, s, nxt;
+
+	f = 1;
+
+	s = 2;
+
+	for (c = 1; c <= 50; ++c)
+	{
+		if (f != 2163150047)
+		{
+			printf("%d, ", f);
+		}
+		else
+		{
+			printf("%d\n", f);
+		}
+		nxt = f + s;
+		f = s;
+		s = nxt;
+	}
+	return (0);
+}
