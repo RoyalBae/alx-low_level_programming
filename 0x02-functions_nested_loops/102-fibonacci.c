be0c36349a53c3bbae8673861c7f27ce6571b18c
RoyalBae <chidinma.sylvia219@gmail.com>
2022-10-19 20:01:11 -0700

    Adpha 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,9 +3,10 @@
 /**
  * main - Program to print the first 50 Fibonacci numbers,
  * starting with 1 and 2
- * and followed by a new line
+ * followed by a new line
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	long int c, f, s, nxt;
